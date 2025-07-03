c60755f1196a0729ab58db63aaa188d163d77585
IreneByron <zhangbingqing7@huawei.com>
2025-07-03 09:31:57 +0800

    hk_tradecal

--- HK/load_data.py
+++ HK/load_data.py
@@ -3,21 +3,33 @@ import time
 from sqlalchemy import create_engine, text
 from datetime import datetime, timedelta
 
-# 需要自己设置本地mysql密码
-with open('../config/mysql_password', 'r') as file:
-    mysql_password = file.read().strip()  
 
-mysql_ip='localhost'
-mysql_port=3306
-database='tushare' # 需要自己新建下database
-engine_ts = create_engine(f"mysql+pymysql://root:{mysql_password}@{mysql_ip}:{mysql_port}/{database}?charset=utf8mb4")
+# mysql配置
+def get_engine_ts():
+    # 需要自己设置本地mysql密码
+    with open('../config/mysql_password', 'r') as file:
+        mysql_password = file.read().strip()  
 
-# 需要自己设置tushare账户token
-with open('../config/tushare_token', 'r') as file:
-    tushare_token = file.read().strip()
+    mysql_ip='localhost'
+    mysql_port=3306
+    database='tushare' # 需要自己新建下database
+    engine_ts = create_engine(f"mysql+pymysql://root:{mysql_password}@{mysql_ip}:   {mysql_port}/{database}?charset=utf8mb4")
+    return engine_ts
 
-ts.set_token(tushare_token)
-pro = ts.pro_api()
+# tushare配置
+def get_pro():
+    # 需要自己设置tushare账户token
+    with open('../config/tushare_token', 'r') as file:
+        tushare_token = file.read().strip()
+
+    ts.set_token(tushare_token)
+    pro = ts.pro_api()
+    return pro
+
+# 全局变量
+engine_ts = get_engine_ts()
+pro=get_pro()
+
 
 # 港股列表
 def hk_basic():
@@ -35,9 +47,20 @@ def hk_basic():
     # print("获取全部暂停上市股票基础信息")
     # df = pro.hk_basic(list_status='L')
 
+
+def hk_tradecal():
+    print("港股交易日")
+    df = pro.hk_tradecal(start_date='20180101', end_date='20211231')
+    df.to_sql('hk_tradecal', engine_ts, index=False, if_exists='replace', chunksize=5000) 
+
+
 def run():
     # 港股列表
-    hk_basic()
+    # hk_basic()
+
+    # 港股交易日
+    hk_tradecal()
+
 
 if __name__ == '__main__':
     run()
